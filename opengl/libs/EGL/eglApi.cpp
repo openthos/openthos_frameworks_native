Bass-ROM/platform_frameworks_native

libEGL: select pixel format by EGL_NATIVE_VISUAL_ID
We need this workaround until Mesa supports HAL_PIXEL_FORMAT_RGBA_8888.

opengl/libs/EGL/eglApi.cpp
@@ -530,7 +530,8 @@ EGLSurface eglCreateWindowSurface(  EGLDisplay dpy, EGLConfig config,
             if (componentType == EGL_COLOR_COMPONENT_TYPE_FIXED_EXT) {
                 if (colorDepth > 24) {
                     format = HAL_PIXEL_FORMAT_RGBA_1010102;
-                } else {
+                } else if (!cnx->egl.eglGetConfigAttrib(iDpy, config, EGL_NATIVE_VISUAL_ID, &format)) {
+                    ALOGE("eglGetConfigAttrib(EGL_NATIVE_VISUAL_ID) failed: %#x", eglGetError());
                     format = HAL_PIXEL_FORMAT_RGBA_8888;
                 }
             } else {

openthos/openthos_frameworks_native

opengl/libs/EGL/eglApi.cpp
@@ -655,7 +655,8 @@ void getNativePixelFormat(EGLDisplay dpy, egl_connection_t* cnx, EGLConfig confi
         if (componentType == EGL_COLOR_COMPONENT_TYPE_FIXED_EXT) {
             if (colorDepth > 24) {
                 format = HAL_PIXEL_FORMAT_RGBA_1010102;
-            } else {
+            } else if (!cnx->egl.eglGetConfigAttrib(dpy, config, EGL_NATIVE_VISUAL_ID, &format)) {
+                ALOGE("eglGetConfigAttrib(EGL_NATIVE_VISUAL_ID) failed: %#x", eglGetError());
                 format = HAL_PIXEL_FORMAT_RGBA_8888;
             }
         } else {